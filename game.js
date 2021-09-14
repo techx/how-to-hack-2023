f0ba7114e4cc55809eacfbb920ed6311f66446f4
Amanda Tong <amanda.g.tong@gmail.com>
2021-09-13 23:25:21 -0400

    updated to match main

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ if(document.getElementById("title").innerHTML == "Hard Mode"){
     max_number = 200;
 }
 
+var score = 0;
+
 getNewProblem();
 
 function getRandomInt(max) {
@@ -10,6 +12,15 @@ function getRandomInt(max) {
 }
 
 function getNewProblem(){
+    var correct = document.getElementById("correct");
+    var wrong = document.getElementById("wrong");
+    var prompt = document.getElementById("prompt");
+
+    correct.removeEventListener("click", correctAnswer);
+    correct.removeEventListener("click", gameOver);
+    wrong.removeEventListener("click", correctAnswer);
+    wrong.removeEventListener("click", gameOver);
+
     var num1 = getRandomInt(max_number);
     var num2 = getRandomInt(max_number);
 
@@ -17,25 +28,23 @@ function getNewProblem(){
 
     var displayed_sol = num1 + num2 + ADJUST[getRandomInt(2)];
 
-    var prompt = document.getElementById("prompt");
     prompt.innerHTML = `${num1} + ${num2} = ${displayed_sol}`
 
-    var correct = document.getElementById("correct");
-    var wrong = document.getElementById("wrong");
-
     if (num1 + num2 == displayed_sol){ // prompt is correct
-        correct.addEventListener("click", function() {
-            getNewProblem();
-        });
-        wrong.addEventListener("click", function() {
-            window.location.replace("/over");
-        });
+        correct.addEventListener("click", correctAnswer);
+        wrong.addEventListener("click", gameOver);
     } else { // prompt isn't correct
-        correct.addEventListener("click", function() {
-            window.location.replace("/over");
-        });
-        wrong.addEventListener("click", function() {
-            getNewProblem();
-        });
+        correct.addEventListener("click", gameOver);
+        wrong.addEventListener("click", correctAnswer);
     }
+}
+
+function correctAnswer() {
+    getNewProblem();
+    score++;
+    document.getElementById('score').innerHTML = `Score: ${score}`
+}
+
+function gameOver(){
+    window.location.replace(`/over/${score}`);
 }
